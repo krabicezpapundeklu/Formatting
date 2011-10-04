b016140931037207946f5fa173828240aefee1a8
krabicezpapundeklu <krabicezpapundeklu@gmail.com>
2011-10-04 14:56:18 +0200

    Self-hosting test runner.

---
+++ tests/Driver.cs
@@ -0,0 +1,44 @@
+﻿namespace Krabicezpapundeklu.Formatting.Tests
+{
+    using System;
+
+    using Gallio.Runner;
+    using Gallio.Runtime;
+    using Gallio.Runtime.ConsoleSupport;
+    using Gallio.Runtime.Logging;
+    using Gallio.Runtime.ProgressMonitoring;
+
+    public static class Driver
+    {
+        public static int Main()
+        {
+            NativeConsole console = NativeConsole.Instance;
+
+            var launcher = new TestLauncher {RuntimeSetup = new RuntimeSetup()};
+
+            launcher.AddFilePattern(typeof(Driver).Assembly.Location);
+            launcher.Logger = new FilteredLogger(new RichConsoleLogger(console), Verbosity.Normal);
+            launcher.RuntimeSetup.AddPluginDirectory(InstallationConfiguration.LoadFromRegistry().InstallationFolder);
+            launcher.TestProject.TestRunnerFactoryName = StandardTestRunnerFactoryNames.Local;
+            launcher.ProgressMonitorProvider = new RichConsoleProgressMonitorProvider(console);
+
+            launcher.EchoResults = true;
+
+            TestLauncherResult result = launcher.Run();
+
+            ConsoleColor originalConsoleColor = console.ForegroundColor;
+
+            console.ForegroundColor = result.ResultCode == ResultCode.Success
+                ? ConsoleColor.Green
+                : ConsoleColor.Red;
+
+            console.WriteLine();
+            console.WriteLine(result.ResultSummary);
+            console.WriteLine();
+
+            console.ForegroundColor = originalConsoleColor;
+
+            return result.ResultCode;
+        }
+    }
+}
