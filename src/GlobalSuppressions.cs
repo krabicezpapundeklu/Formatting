d6ba56c4bdde7c9e7ce1d4786d2c3e8dd2982f0e
krabicezpapundeklu <krabicezpapundeklu@gmail.com>
2011-12-19 10:36:26 +0100

    Suppress CA1704.

---
+++ src/GlobalSuppressions.cs
@@ -0,0 +1,6 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Krabicezpapundeklu", Scope = "namespace", Target = "Krabicezpapundeklu.Formatting.Errors")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Krabicezpapundeklu", Scope = "namespace", Target = "Krabicezpapundeklu.Formatting.Ast")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Krabicezpapundeklu", Scope = "namespace", Target = "Krabicezpapundeklu.Formatting")]
+[assembly: SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = "Krabicezpapundeklu")]
